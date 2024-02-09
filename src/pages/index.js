48357080041d0da47da489305fcce002292ab050
Karan Joisher <karanjoisher@gmail.com>
2024-02-09 12:57:13 -0500

    Completed class activity

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,16 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import MyComp from './qcomps/firstcomp'
+import Bio from './qcomps/bios'
+import ToDo from './qcomps/todos'
+import Gallery from './qcomps/gallery_props'
+import PackingList from './qcomps/props_item'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <PackingList />
     </div>
   )
 }
