fe960cae4322c223b2b083e564ac57bf3987896d
Karan Joisher <karanjoisher@gmail.com>
2024-02-15 15:23:23 -0500

    recepies

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,12 @@
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
 import List from './qcomps/list_keys_id'
+import RecipeList from './qcomps/recipes'
+
 export default function Home() {
   return (
     <div className={styles.main}>
-        <List />
+        <RecipeList />
     </div>
   )
 }
