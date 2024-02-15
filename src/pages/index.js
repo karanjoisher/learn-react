24cbb9b5df06054032e4a17fa9f907291057b717
Karan Joisher <karanjoisher@gmail.com>
2024-02-15 16:02:55 -0500

    Solved state quizes

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,13 @@ import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
 import List from './qcomps/list_keys_id'
 import RecipeList from './qcomps/recipes'
-
+import Gallery from './components/state'
+import FeedbackForm from './qcomps/thankYouCrash'
+import Form from './qcomps/stuckForm'
 export default function Home() {
   return (
     <div className={styles.main}>
-        <RecipeList />
+        <FeedbackForm />
     </div>
   )
 }
